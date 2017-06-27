2d801e26fa665facc868f6612510209637da58ff
alex livenson <alexlivenson1@n003-000-000-000.static.ge.com>
2017-06-27 13:12:23 -0700

    Initial Commit

---
+++ ALSideMenuPresentationController/ALSideMenuPresentationController.h
@@ -0,0 +1,19 @@
+//
+//  ALSideMenuPresentationController.h
+//  ALSideMenuPresentationController
+//
+//  Created by alex livenson on 6/27/17.
+//  Copyright © 2017 alex livenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALSideMenuPresentationController.
+FOUNDATION_EXPORT double ALSideMenuPresentationControllerVersionNumber;
+
+//! Project version string for ALSideMenuPresentationController.
+FOUNDATION_EXPORT const unsigned char ALSideMenuPresentationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALSideMenuPresentationController/PublicHeader.h>
+
+
